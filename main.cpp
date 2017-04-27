3db000d2f61f91d3bd79bbfe3078af0eb4d43504
feilong <614183595@qq.com>
2017-04-27 20:54:24 +0800

    准备分水岭

--- main.cpp
+++ main.cpp
@@ -32,6 +32,58 @@ void DrawGround(Mat src,Mat ret)
 
     //计算连通区域
     std::vector<std::vector<cv::Point> > c;
+    cv::findContours(binMat,c,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
+    std::vector<std::vector<cv::Point> > ca;
+    cv::approxPolyDP(c,ca,1,true);
+
+    //搜索最大连通区
+    double maxArea = 0;
+    size_t MaxI = 0;
+    for(size_t i = 0;i < c.size();i++)
+    {
+        double area = cv::contourArea(c[i]);
+        if(area > maxArea)
+        {
+            maxArea = area;
+            MaxI = i;
+        }
+    }
+
+    //标记
+    RNG rng(12345);
+    ret = src;
+    Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+    drawContours( ret, c, MaxI, color, 2, 8, CV_RETR_EXTERNAL, 0, Point() );
+    drawContours( ret, ca, MaxI, color, 2, 8, CV_RETR_EXTERNAL, 0, Point() );
+    
+    
+
+    imshow("赛道",ret);
+}
+
+
+//画赛道,canny法
+void DrawGround2(Mat src,Mat ret)
+{
+    Mat grayMat;
+    //灰度
+    cv::cvtColor(src,grayMat,CV_BGR2GRAY);
+
+    //动态二值
+    Mat binMat;
+    cv::Canny(grayMat,binMat,15,40);
+    cv::dilate(binMat,binMat,cv::getStructuringElement(MORPH_RECT,Size(10,10)));
+    //cv::erode(binMat,binMat,cv::getStructuringElement(MORPH_RECT,Size(5,5)));
+
+    imshow("canny",binMat);
+
+    cv::waitKey(0);
+    cv::adaptiveThreshold(grayMat,binMat,255,0,0,35,5);
+    cv::dilate(binMat,binMat,cv::getStructuringElement(MORPH_RECT,Size(10,10)));
+    cv::erode(binMat,binMat,cv::getStructuringElement(MORPH_RECT,Size(20,20)));
+
+    //计算连通区域
+    std::vector<std::vector<cv::Point> > c;
     cv::findContours(binMat,c,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
 
     //搜索最大连通区
@@ -52,7 +104,7 @@ void DrawGround(Mat src,Mat ret)
     ret = src;
     Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
     drawContours( ret, c, MaxI, color, 2, 8, CV_RETR_EXTERNAL, 0, Point() );
-    imshow("赛道",ret);
+    imshow("赛道2",ret);
 }
 
 int main ()
@@ -63,6 +115,7 @@ int main ()
 
     Mat r;
     DrawGround(p,r);
+    //DrawGround2(p,r);
 
     std::cout << "end" << std::endl;    
     waitKey(0);
